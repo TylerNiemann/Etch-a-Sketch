79a1f36ba992eb124bcdb565f3887f90080901ca
Tyler Niemann <tntman98@gmail.com>
2022-04-27 11:06:14 -0700

    Complete grid that changes by input

--- etch.js
+++ etch.js
@@ -3,6 +3,10 @@ const container = document.querySelector('#container');
 function createGrid(gridSize){
     for(let i = 0; i< gridSize; i++){
         const row = container.appendChild(document.createElement('div'));
+        for(let j = 0; j < gridSize; j++){
+            const box = row.appendChild(document.createElement('div'));
+            box.classList.add('box')
+        }
     }
 }
-createGrid(5);
+createGrid(16);
