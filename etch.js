8bb7382b8d6035d39573c2f1d043aa4d21651b6e
Tyler Niemann <tntman98@gmail.com>
2022-04-27 13:05:18 -0700

    Add ability for mouseover to give random RGB values instead of black

--- etch.js
+++ etch.js
@@ -1,18 +1,30 @@
 const container = document.querySelector('#container');
 
+function toggleHover(){
+    const grid = document.querySelectorAll('#container > div');
+    grid.forEach(box => box.addEventListener('mouseover', function(e){
+        e.target.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+}));
+}
+
 function createGrid(gridSize){
-    for(let i = 0; i< gridSize; i++){
-        const row = container.appendChild(document.createElement('div'));
-        for(let j = 0; j < gridSize; j++){
-            const box = row.appendChild(document.createElement('div'));
-            box.classList.add('box')
+    if(gridSize <= 100){
+        for(let i = 0; i< gridSize; i++){
+            const row = container.appendChild(document.createElement('div'));
+            for(let j = 0; j < gridSize; j++){
+                const box = row.appendChild(document.createElement('div'));
+                box.classList.add('box')
+            }
         }
     }
+    else alert('Please input a value equal to or less than 100');
 }
+
 function promptMe(){
     let gridSize = parseInt(prompt('Please provide size of grid'));
     reset();
     createGrid(gridSize);
+    toggleHover();
 };
 
 function reset() {
@@ -20,8 +32,3 @@ function reset() {
         container.removeChild(container.firstChild);
       }
   }
-
-const grid = document.querySelectorAll('div > div');
-grid.forEach(box => box.addEventListener('mouseover', function(e){
-     e.target.style.backgroundColor = 'black';
-}));
